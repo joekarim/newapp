2882d0e9d712d6e960dd2bbe71a6ed8b95813d0a
Joe Karim <j.o.karim@gmail.com>
2013-11-18 20:40:40 +0300

    Initial Commit

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -6,3 +6,17 @@ class SayController < ApplicationController
   def goodbye
   end
 end
+
+def say_goodnight(name)
+  result = 'Good night, ' + name
+  return result
+end
+
+# Time for bed...
+puts say_goodnight('Mary-Ellen')
+puts say_goodnight('John-Boy')
+
+def say_goodnight(name)
+  "Good night, #{name.capitalize}"
+end
+puts say_goodnight('pa')
